81b2729e19f0bd4c18d952ab44c4c04ae6eb8caf
maoyl <maoyl@glodon.com>
2024-01-17 14:06:09 +0800

    Day01 vite安装-实现js换成jsx

---
+++ vite-project/App.jsx
@@ -0,0 +1,5 @@
+import React from './core/React';
+
+const App = <div id="myl">hello, maoyl</div>
+
+export default App;
--- vite-project/main.jsx
+++ vite-project/main.jsx
@@ -1,6 +1,43 @@
 import ReactDOM from './core/ReactDOM';
 import React from './core/React';
 
-const App = React.createElement('div', {id: 'app'}, 'hello ','--maoy', ' fj1');
+/*************************************V1分割线**********************************************************/
 
-ReactDOM.createRoot(document.querySelector("#root")).render(App);
+/**
+ * V1 把js转换成jsx
+ */
+// const App = React.createElement('div', {id: 'app'}, 'hello ','maoyl', ' fj1');
+// console.log(AppOne);
+
+// // 没有支持function component 不能直接使用
+// ReactDOM.createRoot(document.querySelector("#root")).render(App);
+
+/*************************************V2分割线**********************************************************/
+
+/**
+ * V2 vite 会子自动把dom转换成React.createElement()对象
+ */
+// const App = React.createElement('div', {id: 'app'}, 'hello ','maoyl', ' fj1');
+// vite 会子自动把dom转换成React.createElement()对象
+// const App = <div id="myl">hello, maoyl</div>
+// console.log(App, 'App--012');
+// function AppOne() {
+//     return  <div id="myl">hello, maoyl</div>
+// }
+// console.log(AppOne);
+
+// // 没有支持function component 不能直接使用
+// ReactDOM.createRoot(document.querySelector("#root")).render(App);
+
+
+/*************************************V3分割线**********************************************************/
+
+/**
+ * V2 vite 会子自动把dom转换成React.createElement()对象
+ * 转换需要使用React（此处我们使用的是我们重写的React.createElement(...)）
+ */
+
+import App from './App'
+// 没有支持function component 不能直接使用
+// error信息【Uncaught DOMException: Failed to execute 'createElement' on 'Document': The tag name provided ('[object Object]') is not a valid name.】
+ReactDOM.createRoot(document.querySelector("#root")).render(<App></App>);
